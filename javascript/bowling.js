2b150099abbfdad8f0ab63772a2b39b228be886c
Josef Jelinek <josef.jelinek@gmail.com>
2011-11-18 11:54:40 +0100

    javascript spare game

--- javascript/bowling.js
+++ javascript/bowling.js
@@ -1,11 +1,34 @@
 var BOWLING_GAME = function () {
   'use strict';
-  var score = 0;
+  var rolls = [];
+
+  function isSpare(index) {
+    return rolls[index] + rolls[index + 1] == 10;
+  }
+
+  function spareBonus(index) {
+    return 10 + rolls[index + 2];
+  }
+
+  function framePins(index) {
+    return rolls[index] + rolls[index + 1];
+  }
+
   return {
     roll: function (pins) {
-      score += pins;
+      rolls.push(pins);
     },
     score: function () {
+      var frame, frameIndex = 0, score = 0;
+      for (frame = 0; frame < 10; frame++) {
+        if (isSpare(frameIndex)) {
+          score += spareBonus(frameIndex);
+          frameIndex += 2;
+        } else {
+          score += framePins(frameIndex);
+          frameIndex += 2;
+        }
+      }
       return score;
     }
   };
