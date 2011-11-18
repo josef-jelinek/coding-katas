a3b091c6242a576d7f582ac05d2d3549e3c1c2e0
Josef Jelinek <josef.jelinek@gmail.com>
2011-11-18 11:58:20 +0100

    javascript strike game

--- javascript/bowling.js
+++ javascript/bowling.js
@@ -2,6 +2,10 @@ var BOWLING_GAME = function () {
   'use strict';
   var rolls = [];
 
+  function framePins(index) {
+    return rolls[index] + rolls[index + 1];
+  }
+
   function isSpare(index) {
     return rolls[index] + rolls[index + 1] == 10;
   }
@@ -10,8 +14,12 @@ var BOWLING_GAME = function () {
     return 10 + rolls[index + 2];
   }
 
-  function framePins(index) {
-    return rolls[index] + rolls[index + 1];
+  function isStrike(index) {
+    return rolls[index] == 10;
+  }
+
+  function strikeBonus(index) {
+    return 10 + rolls[index + 1] + rolls[index + 2];
   }
 
   return {
@@ -21,7 +29,10 @@ var BOWLING_GAME = function () {
     score: function () {
       var frame, frameIndex = 0, score = 0;
       for (frame = 0; frame < 10; frame++) {
-        if (isSpare(frameIndex)) {
+        if (isStrike(frameIndex)) {
+          score += strikeBonus(frameIndex);
+          frameIndex += 1;
+        } else if (isSpare(frameIndex)) {
           score += spareBonus(frameIndex);
           frameIndex += 2;
         } else {
