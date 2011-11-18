df4fe4cbbde67872afd9c27b9709d81262d7b824
Josef Jelinek <josef.jelinek@gmail.com>
2011-11-18 03:53:11 +0100

    clojure spare game

--- bowling-game-test/src/game_test.clj
+++ bowling-game-test/src/game_test.clj
@@ -6,4 +6,8 @@
 (deftest simple-game
   (is (= 20 (score(roll-game (repeat 20 1))))))
 
+(deftest spare-game
+  (let [rolls (concat [5 5 3] (repeat 17 0))]
+	  (is (= 16 (score(roll-game rolls))))))
+
 (run-tests)
--- bowling-game/src/game.clj
+++ bowling-game/src/game.clj
@@ -6,5 +6,19 @@
 (defn roll-game [rolls]
   (reduce roll [] rolls))
 
+(defn- is-spare [game]
+  (= 10 (+ (nth game 0) (nth game 1))))
+
+(defn- spare-bonus [game]
+  (+ 10 (nth game 2)))
+
+(defn- frame-pins [game]
+  (+ (nth game 0) (nth game 1)))
+
 (defn score [game]
-  (reduce + game))
+  (cond
+    (empty? game) 0
+    (is-spare game) (+ (spare-bonus game)
+                       (score (drop 2 game)))
+    :else (+ (frame-pins game)
+             (score (drop 2 game)))))
